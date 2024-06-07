8a0fd213abf2b2e1cfb0e77ef0d8f8c36af35c05
efesxzc <rav4xzc@gmail.com>
2024-06-08 00:13:31 +0500

    some fixes and add some tests

---
+++ tests/fetch_test/fetch_test.cc
@@ -0,0 +1,13 @@
+#include "equeue/queue.hpp"
+
+using namespace efesx::queue;
+
+int main(){
+    queue q;
+
+    q.enqueue(0, 78);
+
+    assert(q.dequeue()->sint32_value(), 78);
+
+    return 0;
+}
